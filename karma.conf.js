fe49a7a07f5a1e39e143776ce56a858f4110289f
Manu Minnig <89226974+Manuminnig@users.noreply.github.com>
2023-11-15 16:24:25 -0300

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -28,8 +28,7 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/app-mi-negocio'),
       subdir: '.',
       reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'cobertura' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
